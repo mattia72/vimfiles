ba47e35a5ff078bb5083ee06a2ec8dab31a0deea
mattia72 <mattiassicha@gmail.com>
2023-03-02 10:23:30 +0100

    new lua mappings, unused parameters

--- lua/utils.lua
+++ lua/utils.lua
@@ -5,11 +5,11 @@ local _M = {}
 function _M.notify_info(title, msg)
   require('notify')(msg,'info', {timeout=2000, title=title})
 end
- 
+
 function _M.create_cmd_with_notify(args)
   setmetatable(args,{__index={opts={nargs=0, bang=true}}})
   vim.api.nvim_create_user_command(args.name,
-    function(opts)
+    function(_)
       vim.cmd(args.cmd)
       _M.notify_info(args.notify_msg)
     end,
@@ -26,7 +26,7 @@ end--
 
 function _M.get_last_modified_in_dir(dir, file_pattern)
   local last_modified
-  for file in io.popen('dir '.. _M.join_paths(dir, file_pattern)..' /b /od'):lines()  do 
+  for file in io.popen('dir '.. _M.join_paths(dir, file_pattern)..' /b /od'):lines()  do
     last_modified  = file
   end
   return last_modified
@@ -36,7 +36,7 @@ end
 function _M.table_count(tbl, cond)
   local count = 0
   local count_cond = 0
-  for key, val in pairs(tbl) do  -- Table iteration.
+  for _, val in pairs(tbl) do  -- Table iteration.
     if(cond(val)) then
       count_cond = count_cond + 1
     end
